62a24aab1215702b4ec690ac366a33881360602b
Victoria Liang <vliang@dementedkitty.com>
2019-09-03 18:29:56 -0700

    Adding base purpose
    To make a loop and array later after verifiying the base idea works

---
+++ update-repos.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+cd ~/Documents/scrum
+git pull origin master
+cd ~/Documents/wip
+git pull origin master
+cd ~/Documents/writing-tools
+git pull origin master
+cd ~/Documents/lazarus
+git pull origin master
+cd ~/Documents/spacefaring-vocabulary
+git pull origin master
+cd ~/Documents/unofficial-forms
+git pull origin master
+cd ~/Documents/writing-exercises
+git pull origin master
+cd ~/Documents/wsl
+git pull origin master
+cd ~/bin/checkmail
+git pull origin master
+cd ~/bin/infopane
+git pull origin master
+cd ~/bin/update-repos
+git pull origin master
+cd ~/bin/vmlab
+git pull origin master
+
