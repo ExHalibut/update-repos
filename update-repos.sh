85b74be2997bbd838ad0b3136e1ba21a7fbeb6da
ExHalibut <exhalibut@dementedkitty.com>
2019-11-02 10:41:59 -0700

    Adding example usage

--- update-repos.sh
+++ update-repos.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
+display_usage() {
+    echo -e "\n Example Usage: ./update-repos ~/"
+    echo -e "\n                ./update-repos ~/bin"
+    echo -e "\n                ./update-repos /opt\n"
+}
+
 topdir=$1
+
+if [  $# -le 1 ]
+then
+	display_usage
+	exit 1
+fi
+
+if [[ ( $# == "--help") ||  $# == "-h" ]]
+then
+	display_usage
+	exit 0
+fi 
 
 for i in $(find $topdir -name ".git"); do
     echo "";
